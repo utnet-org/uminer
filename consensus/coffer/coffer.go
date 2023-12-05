dd419d4abfc596b296a8719aa0f3d8a1c9057e26
jamesavechives <yanhuangpai@126.com>
2023-12-05 17:51:42 +0800

    coffer consensus init

--- consensus/coffer/coffer.go
+++ consensus/coffer/coffer.go
@@ -1,104 +1,16 @@
 package coffer
 
-import (
-	"errors"
-	"fmt"
-	"log"
-	"math/big"
-	"strings"
-
-	"github.com/yanhuangpai/go-utility/common"
-	"github.com/yanhuangpai/go-utility/common/hexutil"
-	"github.com/yanhuangpai/go-utility/core"
-	"github.com/yanhuangpai/go-utility/core/state"
-	"github.com/yanhuangpai/go-utility/crypto"
-	"github.com/yanhuangpai/go-utility/uncdb"
-)
+import "github.com/yanhuangpai/go-utility/core"
 
 // Coffer is the main structure for the Coffer consensus mechanism.
-type Coffer struct {
-	SuperAccount common.Address
-	Signers      []Signer
-	// Other necessary fields...
+type CofferEngine struct {
+	blockchain *core.BlockChain
+	// Other fields as required
 }
 
-// CofferTransaction define a new transaction type in the blockchain.
-// This involves modifying the data structure that represents a transaction.
-type CofferTransaction struct {
-	// Standard transaction fields
-	Nonce    uint64
-	GasPrice *big.Int
-	GasLimit uint64
-	To       *common.Address
-	Value    *big.Int
-	Data     []byte
-	V, R, S  *big.Int
-
-	// Fields specific to coffer
-	Coffer Coffer
-}
-
-// NewCoffer creates a new Coffer consensus mechanism.
-func NewCoffer(genesis *core.Genesis) (*Coffer, error) {
-	superAccount, err := ExtractSuperAccountFromGenesis(genesis)
-	if err != nil {
-		return nil, err
+func New(blockchainInstance *core.BlockChain) *CofferEngine {
+	return &CofferEngine{
+		blockchain: blockchainInstance,
+		// Set other necessary fields
 	}
-
-	return &Coffer{
-		SuperAccount: superAccount,
-		// ... initialize other fields ...
-	}, nil
-}
-
-func NewCofferFromDB(db uncdb.Database) (*state.Coffer, error) {
-	// Retrieve Coffer data from the database
-	// Example: data := db.Get(/* coffer data key */)
-
-	// Initialize and return a new Coffer instance using the retrieved data
-	coffer := &state.Coffer{
-		// Initialize fields using data
-	}
-
-	return coffer, nil
-}
-
-// ChangeSuperAccount updates the super account, need the old super account's private key to sign
-func (c *Coffer) ChangeSuperAccount(signatureHex, newSuperAccount string) error {
-	// Verify the super account
-	result, _ := verifySig(c.SuperAccount.String(), newSuperAccount, signatureHex)
-	if !result {
-
-		return errors.New("only the current super account can update to a new super account")
-	}
-	// Update the super account
-	c.SuperAccount = common.HexToAddress(newSuperAccount)
-	return nil
-}
-
-func verifySig(from, msg, sigHex string) (bool, error) {
-	sig, err := hexutil.Decode(sigHex)
-	if err != nil {
-		err = fmt.Errorf("invalid sig ('%s'), %w", sigHex, err)
-		log.Fatal(err)
-		return false, err
-	}
-
-	messageToHash := fmt.Sprintf("\x19Ethereum Signed Message:\n%d%s", len(msg), msg)
-	msgHash := crypto.Keccak256Hash([]byte(messageToHash))
-	// ethereum "black magic" :(
-	if sig[crypto.RecoveryIDOffset] == 27 || sig[crypto.RecoveryIDOffset] == 28 {
-		sig[crypto.RecoveryIDOffset] -= 27
-	}
-
-	pk, err := crypto.SigToPub(msgHash.Bytes(), sig)
-	if err != nil {
-		err = fmt.Errorf("failed to recover public key from sig ('%s'), %w", sigHex, err)
-		log.Fatal(err)
-		return false, err
-	}
-
-	recoveredAddr := crypto.PubkeyToAddress(*pk)
-	fmt.Printf("recovered address is ('%s')", recoveredAddr)
-	return strings.EqualFold(from, recoveredAddr.Hex()), nil
 }
--- consensus/coffer/consensus.go
+++ consensus/coffer/consensus.go
@@ -1,22 +1,189 @@
 package coffer
 
 import (
-	"github.com/yanhuangpai/go-utility/core/types" // Path to the package where Block is defined
+	"errors"
+	"fmt"
+	"math/big"
+	"time"
+
+	"github.com/yanhuangpai/go-utility/common"
+	"github.com/yanhuangpai/go-utility/common/hexutil"
+	"github.com/yanhuangpai/go-utility/consensus"
+	"github.com/yanhuangpai/go-utility/core/types"
+	"github.com/yanhuangpai/go-utility/crypto"
+	"github.com/yanhuangpai/go-utility/params"
 )
 
-// ValidateBlock validates a block according to Coffer's rules.
-func (c *Coffer) ValidateBlock(block *types.Block) bool {
-	// Implement block validation logic
-	// This might involve checking the block's signer, verifying signatures, etc.
-	return true
+// Coffer proof-of-IC protocol constants.
+var (
+	epochLength = uint64(30000) // Default number of blocks after which to checkpoint and reset the pending votes
+
+	extraVanity = 32                     // Fixed number of extra-data prefix bytes reserved for signer vanity
+	extraSeal   = crypto.SignatureLength // Fixed number of extra-data suffix bytes reserved for signer seal
+
+	nonceAuthVote = hexutil.MustDecode("0xffffffffffffffff") // Magic nonce number to vote on adding a new signer
+	nonceDropVote = hexutil.MustDecode("0x0000000000000000") // Magic nonce number to vote on removing a signer.
+
+	uncleHash = types.CalcUncleHash(nil) // Always Keccak256(RLP([])) as uncles are meaningless outside of PoW.
+
+	diffInTurn = big.NewInt(2) // Block difficulty for in-turn signatures
+	diffNoTurn = big.NewInt(1) // Block difficulty for out-of-turn signatures
+)
+
+// Various error messages to mark blocks invalid. These should be private to
+// prevent engine specific errors from being referenced in the remainder of the
+// codebase, inherently breaking if the engine is swapped out. Please put common
+// error types into the consensus package.
+var (
+	// errUnknownBlock is returned when the list of signers is requested for a block
+	// that is not part of the local blockchain.
+	errUnknownBlock = errors.New("unknown block")
+
+	// errInvalidCheckpointBeneficiary is returned if a checkpoint/epoch transition
+	// block has a beneficiary set to non-zeroes.
+	errInvalidCheckpointBeneficiary = errors.New("beneficiary in checkpoint block non-zero")
+
+	// errInvalidVote is returned if a nonce value is something else that the two
+	// allowed constants of 0x00..0 or 0xff..f.
+	errInvalidVote = errors.New("vote nonce not 0x00..0 or 0xff..f")
+
+	// errInvalidCheckpointVote is returned if a checkpoint/epoch transition block
+	// has a vote nonce set to non-zeroes.
+	errInvalidCheckpointVote = errors.New("vote nonce in checkpoint block non-zero")
+
+	// errMissingVanity is returned if a block's extra-data section is shorter than
+	// 32 bytes, which is required to store the signer vanity.
+	errMissingVanity = errors.New("extra-data 32 byte vanity prefix missing")
+
+	// errMissingSignature is returned if a block's extra-data section doesn't seem
+	// to contain a 65 byte secp256k1 signature.
+	errMissingSignature = errors.New("extra-data 65 byte signature suffix missing")
+
+	// errExtraSigners is returned if non-checkpoint block contain signer data in
+	// their extra-data fields.
+	errExtraSigners = errors.New("non-checkpoint block contains extra signer list")
+
+	// errInvalidCheckpointSigners is returned if a checkpoint block contains an
+	// invalid list of signers (i.e. non divisible by 20 bytes).
+	errInvalidCheckpointSigners = errors.New("invalid signer list on checkpoint block")
+
+	// errMismatchingCheckpointSigners is returned if a checkpoint block contains a
+	// list of signers different than the one the local node calculated.
+	errMismatchingCheckpointSigners = errors.New("mismatching signer list on checkpoint block")
+
+	// errInvalidMixDigest is returned if a block's mix digest is non-zero.
+	errInvalidMixDigest = errors.New("non-zero mix digest")
+
+	// errInvalidUncleHash is returned if a block contains an non-empty uncle list.
+	errInvalidUncleHash = errors.New("non empty uncle hash")
+
+	// errInvalidDifficulty is returned if the difficulty of a block neither 1 or 2.
+	errInvalidDifficulty = errors.New("invalid difficulty")
+
+	// errWrongDifficulty is returned if the difficulty of a block doesn't match the
+	// turn of the signer.
+	errWrongDifficulty = errors.New("wrong difficulty")
+
+	// errInvalidTimestamp is returned if the timestamp of a block is lower than
+	// the previous block's timestamp + the minimum block period.
+	errInvalidTimestamp = errors.New("invalid timestamp")
+
+	// errInvalidVotingChain is returned if an authorization list is attempted to
+	// be modified via out-of-range or non-contiguous headers.
+	errInvalidVotingChain = errors.New("invalid voting chain")
+
+	// errUnauthorizedSigner is returned if a header is signed by a non-authorized entity.
+	errUnauthorizedSigner = errors.New("unauthorized signer")
+
+	// errRecentlySigned is returned if a header is signed by an authorized entity
+	// that already signed a header recently, thus is temporarily not allowed to.
+	errRecentlySigned = errors.New("recently signed")
+)
+
+// Author implements consensus.Engine, returning the Utility address stored
+// from the blockchain state database.
+func (c *CofferEngine) Author(header *types.Header) (common.Address, error) {
+	// Retrieve the current super account from StateDB
+	stateDB, err := c.blockchain.StateAt(header.Root)
+	if err != nil {
+		return common.Address{}, err
+	}
+	return stateDB.Coffer.SuperAccount, nil
 }
 
-// CreateBlock creates a new block to be added to the blockchain.
-func (ctx *Coffer) CreateBlock(parentBlock *types.Block) *types.Block {
-	//	selectedSigner := ctx.SelectSigner()
-	// Implement block creation logic
-	// Select a signer, create the block, sign it, etc.
+func (ce *CofferEngine) VerifyHeader(chain consensus.ChainHeaderReader, header *types.Header) error {
+
+	// Basic checks (timestamp, difficulty, nonce)
+	if err := ce.basicChecks(header); err != nil {
+		return err
+	}
+
+	// Super account verification
+	stateDB, err := ce.blockchain.StateAt(header.Root) // Method to retrieve the current state
+	if err != nil {
+		return err
+	}
+	currentSuperAccount := stateDB.Coffer.SuperAccount
+	if header.Coinbase != currentSuperAccount {
+		return errors.New("block not created by the super account")
+	}
+
+	// Signature verification (if applicable)
+	if err := ce.verifySignature(header, currentSuperAccount); err != nil {
+		return err
+	}
+
+	// Cascading field verification
+	return ce.verifyCascadingFields(chain, header)
+}
+
+func (ce *CofferEngine) basicChecks(header *types.Header) error {
+	// Implement basic checks here
+	number := header.Number.Uint64()
+	// Don't waste time checking blocks from the future
+	if header.Time > uint64(time.Now().Unix()) {
+		return consensus.ErrFutureBlock
+	}
+	// Ensure that the mix digest is zero as we don't have fork protection currently
+	if header.MixDigest != (common.Hash{}) {
+		return errInvalidMixDigest
+	}
+	// Ensure that the block doesn't contain any uncles which are meaningless in PoIC
+	if header.UncleHash != uncleHash {
+		return errInvalidUncleHash
+	}
+	// Ensure that the block's difficulty is meaningful (may not be correct at this point)
+	if number > 0 {
+		if header.Difficulty == nil || (header.Difficulty.Cmp(diffInTurn) != 0 && header.Difficulty.Cmp(diffNoTurn) != 0) {
+			return errInvalidDifficulty
+		}
+	}
+	// Verify that the gas limit is <= 2^63-1
+	if header.GasLimit > params.MaxGasLimit {
+		return fmt.Errorf("invalid gasLimit: have %v, max %v", header.GasLimit, params.MaxGasLimit)
+	}
 	return nil
 }
 
-// Other consensus-related functions...
+func (ce *CofferEngine) verifySignature(header *types.Header, currentSuperAccount common.Address) error {
+	// Implement signature verification logic here
+
+	// Verify the super account
+	if header.Coinbase != currentSuperAccount {
+		return errors.New("block not created by the super account")
+	}
+
+	// Add more checks as needed...
+
+	return nil
+}
+
+// verifyCascadingFields verifies all the header fields that are not standalone,
+// rather depend on a batch of previous headers. The caller may optionally pass
+// in a batch of parents (ascending order) to avoid looking those up from the
+// database. This is useful for concurrently verifying a batch of new headers.
+func (ce *CofferEngine) verifyCascadingFields(chain consensus.ChainHeaderReader, header *types.Header) error {
+	// Implement cascading field checks here (similar to Clique's verifyCascadingFields)
+
+	return nil
+}
