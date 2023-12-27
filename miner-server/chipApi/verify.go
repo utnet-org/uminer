6fcb2592d6a3e9e56849108420be683eb6e6b2ed
JacrominWay <wangjiarong236@gmail.com>
2023-12-27 17:56:30 +0800

    add sign verify at go library

--- miner-server/chipApi/verify.go
+++ miner-server/chipApi/verify.go
@@ -11,9 +11,16 @@ package chipApi
 //
 import "C"
 import (
+	"crypto"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/hex"
+	"encoding/pem"
 	"fmt"
 )
 
+// verify at c++ by the openssl library
 func VerifyMinerChips(signature string, pubK string, signatureSize int, pubKSize int, message string) bool {
 	cSignature := C.CString(signature)
 	cPubK := C.CString(pubK)
@@ -28,29 +35,43 @@ func VerifyMinerChips(signature string, pubK string, signatureSize int, pubKSize
 	return false
 }
 
-//func VerifyChipsSignature(signature string, publicKey string, message string) bool {
-//	// 解析公钥
-//	block, _ := pem.Decode([]byte(publicKey))
-//	if block == nil {
-//		fmt.Println("failed to parse PEM block containing the public key")
-//		return false
-//	}
-//
-//	pubKey, err := x509.ParsePKCS1PublicKey(block.Bytes)
-//	if err != nil {
-//		fmt.Println("failed to parse DER encoded public key: ", err)
-//		return false
-//	}
-//
-//	// 验证签名
-//	data := []byte(message)
-//	digest := sha256.Sum256(data)
-//	err = rsa.VerifyPKCS1v15(pubKey, crypto.SHA256, digest[:], []byte(signature))
-//	if err != nil {
-//		fmt.Println("signature verification failed: ", err)
-//		return false
-//	}
-//
-//	fmt.Println("Signature verified successfully")
-//	return true
-//}
+// verify at go by crypto library
+func hexStringToBytes(hexString string) ([]byte, error) {
+	signatureBytes, err := hex.DecodeString(hexString)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding hex string: %v", err)
+	}
+	return signatureBytes, nil
+}
+func VerifyChipsSignature(signature string, publicKey string, message string) bool {
+	// parse publicKey
+	block, _ := pem.Decode([]byte(publicKey))
+	if block == nil {
+		fmt.Println("failed to parse PEM block containing the public key")
+		return false
+	}
+
+	pubKey, err := x509.ParsePKCS1PublicKey(block.Bytes)
+	if err != nil {
+		fmt.Println("failed to parse DER encoded public key: ", err)
+		return false
+	}
+
+	// Verify sign
+	data := []byte(message)
+	digest := sha256.Sum256(data)
+	signatureBytes, err := hexStringToBytes(signature)
+	if err != nil {
+		fmt.Println("hex signature Error:", err)
+		return false
+	}
+
+	err = rsa.VerifyPKCS1v15(pubKey, crypto.SHA256, digest[:], signatureBytes)
+	if err != nil {
+		fmt.Println("signature verification failed: ", err)
+		return false
+	}
+
+	fmt.Println("Signature verified successfully")
+	return true
+}
